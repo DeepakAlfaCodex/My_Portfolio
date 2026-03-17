965f20b2a3592d1e854acc17246e889264b2315d
DeepakAlfaCodex <ds2900050@gmail.com>
2026-03-17 23:53:13 +0530

    apply ejs

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();  
+const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/home", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
